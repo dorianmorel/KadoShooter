dda5cce22da72ba9ff07c9ec49af0983a1974b36
dorianmorel <moreldorian21@gmail.com>
2023-03-15 09:46:47 +0100

    Deuxième commit : Ajout koopa

--- app/src/main/java/com/example/testmovement/MainActivity.java
+++ app/src/main/java/com/example/testmovement/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -14,25 +15,25 @@ import java.util.concurrent.TimeUnit;
 public class MainActivity extends AppCompatActivity {
 
     private int nbClick = 0;
+    private ImageView button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        button = findViewById(R.id.button);
 
     }
 
     public void move(View v) {
         System.out.println("CLICK");
-        Button button = findViewById(R.id.button);
         nbClick+=1;
 
         if (nbClick == 1) {
             Runnable helloRunnable = new Runnable() {
                 public void run() {
-                    button.setTop(button.getTop()-1);
-                    button.setBottom(button.getBottom()-1);
+                    button.setTop(button.getTop()+1);
+                    button.setBottom(button.getBottom()+1);
                     button.setLeft(button.getLeft()+1);
                     button.setRight(button.getRight()+1);
                 }
