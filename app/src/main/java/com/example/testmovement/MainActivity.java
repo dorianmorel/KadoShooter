7388fd9b6f8380434b332e22e3e2e3a04923efd3
dm546591 <moreldorian21@gmail.com>
2023-03-17 08:25:42 +0100

    Title hide

--- app/src/main/java/com/example/testmovement/MainActivity.java
+++ app/src/main/java/com/example/testmovement/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -33,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
