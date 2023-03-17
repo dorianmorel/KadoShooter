4a82a32586b9137ef5b2f7bf4b667aef8f0b760d
dm546591 <moreldorian21@gmail.com>
2023-03-17 11:23:17 +0100

    Optimize direction + Accueil

---
+++ app/src/main/java/com/example/testmovement/StageOne.java
@@ -0,0 +1,109 @@
+package com.example.testmovement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class StageOne extends AppCompatActivity {
+
+    private int nbClick = 0;
+    private ImageView button;
+    private ArrayList<GifImageView> gifs = new ArrayList<>();
+    private ArrayList<Double> directions = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_stage_one);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int displayWidth = displayMetrics.widthPixels;
+        int displayHeight = displayMetrics.heightPixels;
+
+        RelativeLayout ecran = (RelativeLayout) findViewById(R.id.ecran);
+
+        for (int i=0; i < 25; i++) {
+
+            GifImageView gif = new GifImageView(this);
+            gif.setImageResource(R.drawable.koopa);
+            gif.setLayoutParams(new RelativeLayout.LayoutParams(200,200));
+
+            gif.setX((int)(Math.random() * (displayWidth-200)));
+            gif.setY((int)(Math.random() * (displayHeight-200)));
+
+            gif.setOnClickListener(v -> {
+                Log.i("AAAA", "CLICK");
+                //v.setVisibility(View.GONE);
+                ViewGroup parentView = (ViewGroup) v.getParent();
+                parentView.removeView(v);
+            });
+
+            double direction = degree2Radian(Math.random() * 360); // angle aléatoire entre 0 et 360° (mais en radians)
+            //double direction = degree2Radian(45);
+
+            ecran.addView(gif);
+            gifs.add(gif);
+            directions.add(direction);
+        }
+
+
+        Runnable movements = new Runnable() {
+            public void run() {
+
+                for (int i = 0; i < gifs.size(); i++) {
+
+                    GifImageView gif = gifs.get(i);
+                    double direction = directions.get(i);
+
+                    float x = gif.getX();
+                    float y = gif.getY();
+
+                    float speed = 10;
+
+                    x += Math.cos(direction) * speed;
+                    y += Math.sin(direction) * speed;
+
+                    gif.setX(x);
+                    gif.setY(y);
+
+                    if (x >= displayWidth-200 || x <= 0)
+                        directions.set(i, degree2Radian(180) - direction);
+                    else if (y >= displayHeight-200 || y <= 0)
+                        directions.set(i, -direction);
+                }
+            }
+        };
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(movements, 0, 20, TimeUnit.MILLISECONDS);
+    }
+
+    private double degree2Radian(double degree) {
+        return degree * Math.PI / 180;
+    }
+
+    public void clicEcran(View v){
+        Log.i("AAAA", "CLIC ECRAN");
+    }
+}
