134981449c84957c14e21f821c9d158138f79d63
Dorian MOREL <83545355+dorianmorel@users.noreply.github.com>
2023-04-27 14:50:51 +0200

    Comments added on Credits.java

--- app/src/main/java/com/example/kadoshooter/Credits.java
+++ app/src/main/java/com/example/kadoshooter/Credits.java
@@ -6,10 +6,14 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Window;
 
+//Cette activité correspond à la page des Crédits du jeu
 public class Credits extends AppCompatActivity {
-
+    
     private MediaPlayer creditsTheme;
     @Override
+    
+    
+    //On crée l'écran et on lance la musique
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -22,10 +26,11 @@ public class Credits extends AppCompatActivity {
 
         setContentView(R.layout.activity_credits);
     }
-
+    
+    //Met en pause la musique si on quitte l'activité
     @Override
     protected void onPause() {
-        super.onPause();  // Always call the superclass method first
+        super.onPause();
         creditsTheme.stop();
     }
 }
