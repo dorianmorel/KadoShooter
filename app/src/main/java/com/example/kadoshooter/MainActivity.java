dce2e5d88faa008e652c0559c25ebd07324a387e
Dorian MOREL <83545355+dorianmorel@users.noreply.github.com>
2023-04-27 14:54:09 +0200

    Comments added on MainActivity.java

--- app/src/main/java/com/example/kadoshooter/MainActivity.java
+++ app/src/main/java/com/example/kadoshooter/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
-
+//Ecran d'accueil du jeu
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mainTheme;
@@ -25,23 +25,31 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
     }
-
+    
+    //Lancement du premier niveau
     public void onClickStage1(View view) {
         Intent intent = new Intent(this, StageOne.class);
         startActivity(intent);
     }
+    
+    //Lancement du deuxième niveau
     public void onClickStage2(View view) {
         Intent intent = new Intent(this, StageTwo.class);
         startActivity(intent);
     }
+    
+    //Lancement du troisième niveau
     public void onClickStage3(View view) {
         Intent intent = new Intent(this, StageThree.class);
         startActivity(intent);
     }
+    
+    //Lancement de l'écran des crédits
     public void onClickCredits(View view) {
         Intent intent = new Intent(this, Credits.class);
         startActivity(intent);
     }
+    
     @Override
     protected void onResume() {
         super.onResume();  // Always call the superclass method first
