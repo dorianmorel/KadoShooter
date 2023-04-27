1ce2b02d477b8330bdf3323fd4ec8e89f6ac6ca3
Dorian MOREL <83545355+dorianmorel@users.noreply.github.com>
2023-04-27 14:52:55 +0200

    Comments added on Ennemi.java

--- app/src/main/java/com/example/kadoshooter/Ennemi.java
+++ app/src/main/java/com/example/kadoshooter/Ennemi.java
@@ -4,12 +4,14 @@ import android.content.Context;
 
 import pl.droidsonroids.gif.GifImageView;
 
+//Classe correspondant à un ennemi, quel qu'il soit
 public class Ennemi {
     private GifImageView gif;
     private Double direction;
     private Context context;
     private float speed;
-
+    
+    //Création de l'ennemi grâce au gif associé, à sa direction, et sa vitesse initiale
     public Ennemi(Context context, GifImageView file, Double direction, float speed){
         this.context=context;
 
@@ -19,29 +21,37 @@ public class Ennemi {
 
         this.speed = speed;
     }
-
+    
+    //Retourne le gif
     public GifImageView getGif() {
         return this.gif;
     }
+    
+    //Retourne la direction
     public Double getDirection() {
         return this.direction;
     }
-
+    
+    //Donne un fichier Gif à l'ennemi
     public void setGif(float x, float y) {
         this.gif.setX(x);
         this.gif.setY(y);
     }
-
+    
+    //Donne une direction à l'ennemi
     public void setDirection(Double direction) {
         this.direction = direction;
     }
-
+    
     public String toString() {
         return "[Ennemi X="+this.gif.getX()+" Y="+this.gif.getY()+"]";
     }
-
+    
+    //Modification de la vitesse de l'ennemi
     public void setSpeed(float speed) {
         this.speed = speed;
     }
+    
+    //Retourne la vitesse
     public float getSpeed() { return this.speed; }
 }
